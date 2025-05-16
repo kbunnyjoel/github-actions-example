95e9ac7a358482f500ef50e77cfa9a7a9fd25f96
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-16 13:36:36 +1000

    NPM CI start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const math = require('./math');
 
-const num1 = Number(process.env.INPUT_NUMBER1 || 0);
-const num2 = Number(process.env.INPUT_NUMBER2 || 0);
+const num1 = Number(process.env.NUMBER1 || 0);
+const num2 = Number(process.env.NUMBER2 || 0);
 
 console.log(`Adding ${num1} and ${num2}`);
 console.log('Result:', num1 + num2);
