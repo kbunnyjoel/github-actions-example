d5c91984063cd37d374808e0fc0fcc244a99c68a
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-25 00:51:13 +1000

    Fixed EKS public subnets and s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
     bucket        = "my-eks-terraform-state"
     key           = "eks/terraform.tfstate"
     region        = "ap-southeast-2"
-    use_lock_file = true # NEW in v1.3+
+    lock_file = true # NEW in v1.3+
     encrypt       = true
   }
 }
