69cd1bd97fb95bd860861609a336c4dc7bd482bf
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-26 03:22:04 +1000

    Fix

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -65,6 +65,6 @@ module "eks" {
   # }
 
   enable_irsa = true
-  enable_cluster_creator_admin_permissions = true
-  authentication_mode = "API_AND_CONFIG_MAP"
+  # enable_cluster_creator_admin_permissions = true
+  # authentication_mode = "API_AND_CONFIG_MAP"
 }
