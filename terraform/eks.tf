1681cd73913b7f2115464702c9d37fc6b55cefaf
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-26 01:39:19 +1000

    Spot instances

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -40,26 +40,29 @@ module "eks" {
   cluster_version = "1.29"
   subnet_ids      = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
+  # API access settings
+  cluster_endpoint_public_access  = false  # or true to enable public API access
+  cluster_endpoint_private_access = true
 
-  # eks_managed_node_groups = {
-  #   spot-nodes = {
-  #   desired_size   = 1
-  #   min_size       = 0
-  #   max_size       = 2
-  #   instance_types = ["t3.small", "t3.medium", "t3a.small"]
-  #   capacity_type  = "SPOT"
-  #   }
-  # }
-  
-    eks_managed_node_groups = {
-    default-on-demand = {
+  eks_managed_node_groups = {
+    spot-nodes = {
     desired_size   = 1
-    min_size       = 1
+    min_size       = 0
     max_size       = 2
-    instance_types = ["t3.medium"]
-    capacity_type  = "ON_DEMAND"
+    instance_types = ["t3.small", "t3.medium", "t3a.small"]
+    capacity_type  = "SPOT"
     }
   }
+  
+  #   eks_managed_node_groups = {
+  #   default-on-demand = {
+  #   desired_size   = 1
+  #   min_size       = 1
+  #   max_size       = 2
+  #   instance_types = ["t3.medium"]
+  #   capacity_type  = "ON_DEMAND"
+  #   }
+  # }
 
   enable_irsa = true
 }
