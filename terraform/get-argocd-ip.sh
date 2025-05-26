6b83f4487e3add1e1e7ffb604bb95d133dd4def1
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-26 16:24:31 +1000

    ARGOCD DNS Fix

--- terraform/get-argocd-ip.sh
+++ terraform/get-argocd-ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-IP=$(kubectl get svc -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+IP=$(kubectl get svc argocd-server -n argocd -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 echo "{\"argocd_ip\": \"$IP\"}"
