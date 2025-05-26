77ce3877a4a3afee1a16a3a3ffbcd47c93c096a6
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-26 21:45:27 +1000

    ArgoCD

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,9 +7,9 @@ output "eks_cluster_name" {
   value       = module.eks.cluster_id
 }
 
-output "argocd_ip" {
-  value = data.external.argocd_ip.result["argocd_ip"]
-}
+# output "argocd_ip" {
+#   value = data.external.argocd_ip.result["argocd_ip"]
+# }
 
 # Output bastion IP
 # output "bastion_public_ip" {
