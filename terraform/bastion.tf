2c7666feef49057d23ee2f939e9b8b445e8fff5c
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-28 16:25:53 +1000

    ArgoCD

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "bastion_sg" {
 # Bastion EC2 instance in public subnet
 resource "aws_instance" "bastion" {
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
+  instance_type          = "t3.small"
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   key_name               = aws_key_pair.deployment_key.key_name
