62716e68d86db664daf8d5355c6f86f2b76b9afb
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-06 13:06:01 +1000

    ArgoCD deployment

--- terraform/output.tf
+++ terraform/output.tf
@@ -16,14 +16,3 @@ output "bastion_public_ip" {
   description = "The static Elastic IP address of the bastion host."
   value       = aws_eip.bastion_eip.public_ip
 }
-
-
-output "ingress_hostname" {
-  value       = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].hostname
-  description = "The hostname of the ingress ELB"
-}
-
-output "ingress_zone_id" {
-  value       = data.aws_elb_service_account.main.zone_id
-  description = "The hosted zone ID for the ingress ELB"
-}
