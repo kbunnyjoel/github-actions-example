f26d28fee86e710a26f5c508f0be1ab7b261997b
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-08 23:25:36 +1000

    fix code

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -78,6 +78,7 @@ module "eks" {
   # API access settings
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
+  cluster_endpoint_public_access_cidrs   = ["140.82.112.0/20", "185.199.108.0/22"]  # GitHub Actions IP ranges  
 
   # Add CloudWatch logging for worker nodes
   cloudwatch_log_group_kms_key_id        = aws_kms_key.eks.arn
