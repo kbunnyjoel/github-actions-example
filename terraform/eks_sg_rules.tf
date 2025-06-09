dd36fb5ff29601bcf1f3833f5f35dbb87d7d53b5
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-09 19:17:32 +1000

    Code changes

--- terraform/eks_sg_rules.tf
+++ terraform/eks_sg_rules.tf
@@ -1,15 +1,7 @@
 # Additional security group rules for EKS cluster
 # These rules allow specific inbound traffic that was previously managed via GitHub Actions
 
-resource "aws_security_group_rule" "allow_https" {
-  description       = "Allow HTTPS inbound from GitHub Actions"
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["140.82.112.0/20"] # GitHub Actions IP range
-  security_group_id = module.eks.cluster_security_group_id
-}
+# GitHub Actions HTTPS access is now handled by cluster_endpoint_public_access_cidrs in eks.tf
 
 resource "aws_security_group_rule" "allow_argocd" {
   description       = "Allow ArgoCD UI access"
