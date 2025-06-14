a6d31cc0cd86da30b612af18f6e0227ea482d098
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-15 00:14:02 +1000

    Cognito check

--- terraform/external_dns.tf
+++ terraform/external_dns.tf
@@ -10,14 +10,14 @@ resource "aws_iam_policy" "external_dns" {
         Action = [
           "route53:ChangeResourceRecordSets"
         ],
-        Resource = [aws_route53_zone.main.arn] // Should be arn:aws:route53:::hostedzone/Z0722775AO3VTQLFVQU3
+        Resource = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.main.zone_id}"] // Should be arn:aws:route53:::hostedzone/Z0722775AO3VTQLFVQU3
       },
       {
         Effect = "Allow",
         Action = [
           "route53:ListResourceRecordSets" // Permission is present
         ],
-        Resource = [aws_route53_zone.main.arn] // Should be arn:aws:route53:::hostedzone/Z0722775AO3VTQLFVQU3
+        Resource = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.main.zone_id}"] // Should be arn:aws:route53:::hostedzone/Z0722775AO3VTQLFVQU3
       },
       {
         Effect = "Allow",
