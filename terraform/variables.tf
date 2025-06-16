8bbc79a08e09e6a0f5b1eefecaeb9679b646b6ad
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-16 12:29:41 +1000

    Apply TF code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,9 +32,11 @@ variable "create_dns_records" {
 variable "acm_cert_arn" {
   description = "ARN of the ACM certificate for custom Cognito domain"
   type        = string
+  default     = ""
 }
 
 variable "cognito_custom_domain" {
   description = "The custom domain to associate with the Cognito user pool (e.g., auth.bunnycloud.xyz)"
   type        = string
+  default     = ""
 }
