96e0db66c2c5633b3367348814a0533e0da192e6
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-21 02:33:19 +1000

    AUtoscaler

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -20,13 +20,3 @@ resource "aws_iam_role" "alb_controller_role" {
     ]
   })
 }
-
-resource "kubernetes_service_account" "alb_controller_sa" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller_role.arn
-    }
-  }
-}
