defa017fa48a9767f6fab312cae9d006844510d5
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-21 06:08:42 +1000

    AUtoscaler

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -31,8 +31,8 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.github_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.github_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.github_auth.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.github_cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.github_cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.github_auth.token
+# }
