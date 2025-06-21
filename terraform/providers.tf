9b9e90ca3c11874a67f6bf67e3a00ac0fbecffc7
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-21 20:39:33 +1000

    ASG scaling IAM Role

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -56,7 +56,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.github_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.github_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.github_auth.token
-  config_path            = "~/.kube/config"
 }
 
 provider "helm" {
