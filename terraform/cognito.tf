454977426c2038cfee1d3cd3e489a2810c1a313e
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-22 14:36:58 +1000

    TF destroy all

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -173,7 +173,7 @@ resource "aws_cognito_user_in_group" "dev_in_dev_group" {
 
 # Store the Cognito client secret in AWS Secrets Manager
 resource "aws_secretsmanager_secret" "argocd_cognito_client_secret" {
-  name        = "argocd-cognito-client-secret-v6" # This is the secret ID your workflow expects
+  name        = "argocd-cognito-client-secret-v7" # This is the secret ID your workflow expects
   description = "Client secret for ArgoCD Cognito User Pool Client"
 }
 
