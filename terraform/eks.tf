6df84e548fe508e84054e6bcca264a6dc4487dc6
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-23 02:08:47 +1000

    Install NGINX issue

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -203,8 +203,8 @@ module "eks" {
       desired_size      = 1
       min_size          = 1
       max_size          = 3
-      instance_types    = ["t4g.large", "c6a.large"]
-      ami_type          = "AL2_ARM_64"
+      instance_types    = ["t4g.large", "t3a.large", "c6a.large"]
+      ami_type          = "AL2_x86_64"
       capacity_type     = "SPOT"
       key_name          = aws_key_pair.deployment_key.key_name
       enable_monitoring = true
