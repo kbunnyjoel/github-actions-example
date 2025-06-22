722a0ff9a87ea0638c039739b20e664616d76835
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-22 19:35:11 +1000

    Ingress SSL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -32,7 +32,7 @@ output "external_dns_role_arn" {
   value       = aws_iam_role.external_dns.arn
 }
 
-output "route53_zone_id"  {
+output "route53_zone_id" {
   description = "The ID of the Route 53 hosted zone"
   value       = aws_route53_zone.main.zone_id
 }
