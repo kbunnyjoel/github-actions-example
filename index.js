eb4099ae41bece26f235d68c126f69c58e9b635a
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-06-23 11:24:48 +1000

    Deploy the application for Graviton

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
   console.log('Received API Key:', apiKey);
   console.log('Expected API Key:', expectedKey);
 
-  const isExempt = exemptPaths.some(p => req.path.startsWith(p));
+  const isExempt = exemptPaths.some(p => req.path === p);
   if (isExempt) {
     return next();
   }
